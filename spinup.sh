3754646aeb236552547a6a85bf5726f4298250a5
quinn snyder <snyderq@gmail.com>
2020-01-29 18:02:17 +0100

    brute force the re-init

--- spinup.sh
+++ spinup.sh
@@ -2,6 +2,8 @@ pip install jinja2==2.10.3
 pip install virlutils
 virl pull securenetwrk/4_router_mesh
 ansible-playbook vpn.yml
+virl use $(virl ls | grep -o "mycode[^ ]*")
+virl down
 virl up --provision
 virl nodes | grep ERROR | awk '{print $2}' | while read line; do
 echo $line
