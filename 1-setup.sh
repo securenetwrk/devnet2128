8c217d85f0ea7a82bab0a9b2c9cbe41ee8491eea
quinn snyder <snyderq@gmail.com>
2020-01-29 16:51:58 +0100

    fixing virl id issue

--- 1-setup.sh
+++ 1-setup.sh
@@ -1,6 +1,6 @@
 pip install jinja2==2.10.3
 pip install virlutils
 ansible-playbook vpn.yml
-virl use $(virl id)
+virl use $(virl ls | grep -o "mycode[^ ]*")
 virl generate ansible
 cp default_inventory.yaml inventory
